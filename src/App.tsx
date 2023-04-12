e387398ea43a885a665e7778bae49b20b8a1d7b7
wollens <wollens123@163.com>
2023-04-12 10:16:32 +0800

    :sparkles: feat: react-route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-gray-500">Hello tailwind</h1>
+    <>
+      <h1 className="text-3xl font-bold underline bg-gray-500">
+        Hello tailwind
+      </h1>
+    </>
   );
 }
 
